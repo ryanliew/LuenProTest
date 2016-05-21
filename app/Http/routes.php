51267b8998b4c2bb1dabd2df9f17f5bf1426defe
wtianhaw94 <wtianhaw94@hotmail.com>
2016-05-21 18:44:20 +0800

    feature#6

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -32,3 +32,8 @@ Route::get('/heaven', function () {
 Route::get('/sea', function () {
     return view('welcome');
 });
+
+//feature/#6--Route to sea--Ryan
+Route::get('/sea', function () {
+    return view('welcome');
+});
